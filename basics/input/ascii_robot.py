51742a6d94c11ab5b18e1a4a61f1c0578c90d8d9
repl.it user <replituser@example.com>
2020-10-18 10:35:27 +0000

    added ascii_robot.py

---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,2 @@
+eye = input("Please enter a character for the eye\n")
+print("Beep's expression is now as follows:\n##########\n#  "+eye+"  "+eye+"  #\n#  ----  #\n##########")
---
+++ basics/input/user_input.py
@@ -0,0 +1,5 @@
+# Ask user to enter their name
+name = input("What is your name human?\n")
+print("It is nice to meet you, "+ name + ".\n")
+
+print("Nice to meet you, {}.".format(name))
