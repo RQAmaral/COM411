e11c43c36573e522b528fa54e68900caa8bae3c4
repl.it user <replituser@example.com>
2020-10-18 10:38:35 +0000

    modified code

--- basics/input/user_input.py
+++ basics/input/user_input.py
@@ -1,5 +1,9 @@
 # Ask user to enter their name
 name = input("What is your name human?\n")
+<<<<<<< HEAD
 print("It is nice to meet you, "+ name + ".\n")
 
 print("Nice to meet you, {}.".format(name))
+=======
+print("It is nice to meet you human", name, ".\n")
+>>>>>>> origin/main
