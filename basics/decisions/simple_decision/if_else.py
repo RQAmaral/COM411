a9745b23575d22bb71830b33eb96356d90f6f059
repl.it user <replituser@example.com>
2020-10-20 11:06:32 +0000

    added if_else.py

---
+++ basics/decisions/simple_decision/if_else.py
@@ -0,0 +1,8 @@
+activity_type = input("Please enter the activity to be performed.\n")
+
+if activity_type == "calculate":
+  print("Performing calculations...")
+else:
+  print("Performing activity...")
+
+print("Activity completed!")
