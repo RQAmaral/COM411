1c4a20cc778d44e5985030f0bc99de49849df4c8
repl.it user <replituser@example.com>
2020-11-05 01:07:33 +0000

    added and_operator.py

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,7 @@
+sound = input("What did I hear?\n")
+view = input("What did I see?\n")
+
+if sound == "grr" and view == "two red eyes":
+  print("There is a scary creature. I should get out of here!")
+else:
+  print("I am a little scared but I will continue.")
